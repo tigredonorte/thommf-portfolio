e31143dc1b4534546a067aae15643618b8a544e1
Thompson <tigredonorte3@gmail.com>
2025-07-11 11:56:11 -0300

    fine tunning the deployment

--- infra/terraform/iam.tf
+++ infra/terraform/iam.tf
@@ -148,3 +148,67 @@ resource "aws_iam_policy" "frontend_deployer_policy" {
     Project     = "thommf-portfolio"
   }
 }
+
+# GitHub Actions OIDC Provider
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  count = var.create_iam_resources && var.use_oidc ? 1 : 0
+
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd"
+  ]
+
+  tags = {
+    Name        = "GitHub Actions OIDC Provider"
+    Environment = var.resource_tag_environment
+    Project     = "thommf-portfolio"
+  }
+}
+
+# GitHub Actions Role
+resource "aws_iam_role" "github_actions_role" {
+  count = var.create_iam_resources && var.use_oidc ? 1 : 0
+
+  name = "GitHubActionsRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github_actions[0].arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repository}:*"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name        = "GitHub Actions Role"
+    Environment = var.resource_tag_environment
+    Project     = "thommf-portfolio"
+  }
+}
+
+# Attach the frontend deployer policy to GitHub Actions role
+resource "aws_iam_role_policy_attachment" "github_actions_role_policy" {
+  count = var.create_iam_resources && var.use_oidc ? 1 : 0
+
+  role       = aws_iam_role.github_actions_role[0].name
+  policy_arn = aws_iam_policy.frontend_deployer_policy[0].arn
+}
